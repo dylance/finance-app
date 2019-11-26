9570e0b45ec325cd7f6bf440a77b82d58f5ddf81
Dylan Ellison <dylancellison@gmail.com>
2019-11-25 16:22:38 -0800

    bug save id key with underscore

--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -3,13 +3,11 @@ import { GET_USER, CLEAR_USER }  from '../actions/types'
 const INITIAL_STATE = {};
 
 export default function(state = INITIAL_STATE, action) {
-  console.log("The action payload isss: ", action)
   switch(action.type) {
     case GET_USER:
       const { firstName, lastName, _id } = action.payload
-      return { firstName, lastName, id: _id}
+      return { firstName, lastName, _id: _id}
     case CLEAR_USER:
-    console.log("this should be run")
       return {}
     default:
       return state;
