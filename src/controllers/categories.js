df0814a30d618030896c4153f42087ea077c139a
Dylan Ellison <dylancellison@gmail.com>
2019-11-25 16:34:00 -0800

    Clean up categories controller

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -2,7 +2,7 @@ const Categories = require('../models/categories');
 
 exports.createCategory = function(req, res, next) {
   const { category, _id } = req.body;
-  console.log("The id is: ", _id, category)
+
   const categories = new Categories({
     category,
     _user: _id
@@ -13,21 +13,19 @@ exports.createCategory = function(req, res, next) {
       return next(err);
     }
 
-    res.json({ token: 'categories saved!!' });
+    res.json({ sucess: 'categories saved!!' });
   });
 };
 
 exports.getCategories = async function(req, res, next) {
   const { _id }  = req.body
-  console.log("The iddd is: ", req.body._id)
 
-  const categories = await Categories.find({_user: _id })
-  console.log("The categories are: ", categories)
-  res.send(categories)
+  if ( _id ) {
+    const categories = await Categories.find({_user: _id })
+    res.send(categories)
+  }
+
+   res.status(400).send({
+     message: 'User id required to make this request'
+   });
 }
-
-
-//5dc8eeca6043412db6b41cb7
-
-
-// dylan@dylan.com user id - 5db1c1344e670a3c98f8f984
