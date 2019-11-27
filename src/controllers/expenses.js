c1de8fc80bb38cd15907ff7a9354254b33baa46f
Dylan Ellison <dylancellison@gmail.com>
2019-11-27 11:54:01 -0800

    Create categories query and user sign up mutation

--- src/controllers/expenses.js
+++ src/controllers/expenses.js
@@ -3,7 +3,7 @@ const Expenses = require('../models/expenses');
 exports.createExpense = function(req, res, next) {
   const { label, description, spendDate, ammount, userId, catId } = req.body;
 
-//  const id = '5db1c1344e670a3c98f8f984';
+  //  const id = '5db1c1344e670a3c98f8f984';
   //const catId = '5db21175acbb7948642c3e32';
 
   // dylan2 id: "5ddcac3cb7ab95cbb09d2168"
@@ -22,7 +22,7 @@ exports.createExpense = function(req, res, next) {
     _user: userId,
   });
 
-  console.log("The expsene is: ", expense)
+  console.log('The expsene is: ', expense);
 
   expense.save(function(err) {
     if (err) {
@@ -34,14 +34,15 @@ exports.createExpense = function(req, res, next) {
 };
 
 exports.getExpenses = async function(req, res, next) {
-  const { _id } = req.body
-  console.log("The id is", _id)
+  const { _id } = req.body;
+  console.log('The id is', _id);
   //const id = '5ddcac3cb7ab95cbb09d2168';
   //const catId = '5db21175acbb7948642c3e32';
   // fuck category 5dde4743a1e48f0ba483f8d4
 
   //const expenses = await Expenses.find({ _user: id }).populate('5dde4743a1e48f0ba483f8d4');
-  const expenses = await Expenses.find({ _user: _id, _category: '5dde4743a1e48f0ba483f8d4'  })
+  //const expenses = await Expenses.find({ _user: _id, _category: '5dde4743a1e48f0ba483f8d4'  })
+  const expenses = await Expenses.find({ _user: _id });
 
   res.send(expenses);
 };
--- src/schema/index.js
+++ src/schema/index.js
@@ -5,10 +5,9 @@ const Categories = require('../models/categories');
 const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
   type User {
     email: String
-    id: String
+    id: ID
     firstName: String
     lastName: String
     what: String
@@ -17,11 +16,18 @@ const typeDefs = gql`
 
   type Categories {
     category: String
-    user: String
+    id: ID
   }
 
   input userInput {
     email: String
+  }
+
+  input signUpInput {
+    email: String
+    password: String
+    firstName: String
+    lastName: String
   }
 
   # The "Query" type is special: it lists all of the available queries that
@@ -29,15 +35,23 @@ const typeDefs = gql`
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     user(email: String): User
+    categories(userId: ID): [Categories]
+  }
+
+  type Mutation {
+    signUp(input: signUpInput): User
   }
 `;
 
 console.log('The dirname is: ', __dirname);
-// Categories.find({_user: id })
 
 const resolvers = {
   Query: {
     user: (parent, args) => User.findOne({ email: args.email }),
+    categories: (parent, args) => Categories.find({ userId: args.id }),
+  },
+  Mutation: {
+    signUp: (parent, { input }) => signUp(input),
   },
   User: {
     categories(parent) {
@@ -45,6 +59,24 @@ const resolvers = {
       return Categories.find({ _user: parent.id });
     },
   },
+  Categories: {},
 };
+
+function signUp({ email, password, firstName, lastName }) {
+  const user = new User({
+    email,
+    password,
+    firstName,
+    lastName,
+  });
+
+  user.save(function(err) {
+    if (err) {
+      console.log('The error is: ', err);
+      return err;
+    }
+  });
+  return user;
+}
 
 module.exports = { typeDefs, resolvers };
